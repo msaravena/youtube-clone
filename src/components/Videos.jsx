9fe6a53fb51568980b15a8f4890e609098e969e8
Marcos A <aravenap@gmail.com>
2023-06-12 16:41:20 -0400

    nothing new added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -9,8 +9,8 @@ const Videos = ({ videos, direction }) => {
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item}/>}          
           {item.id.channelId && <ChannelCard channelDetail={item}/>}          
+          {item.id.videoId && <VideoCard video={item}/>}          
         </Box> 
       ))}
     </Stack>
